059d273647282f02da014f62ef24c689d5101908
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-04-03 12:47:16 +0800

    Initial functions in place

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+// Web3 tools
+import { useWeb3React } from "@web3-react/core"
+import { injected } from '../app/wallet/Connector'
+
 // Sections 
 import HarvestForm from '../sections/HarvestForm'
 import TopMenu from '../sections/TopMenu'
@@ -10,6 +14,31 @@ import Summary from '../sections/summary'
 import StakeForm from '../sections/StakeForm'
 
 export default function Home() {
+
+  const { active, account, library, connector, activate, deactivate } = useWeb3React()
+
+  async function connect() {
+    try {
+      console.log('Connecting wallet ...')
+      await activate(injected)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function disconnect() {
+    try {
+      console.log('DISCONNECTING WALLET')
+      // deactivate()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const testMe = () => {
+    console.log('oieufoeufoeufo')
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -17,8 +46,8 @@ export default function Home() {
         <meta name="description" content="OPPA Token Staking" />
         <link rel="icon" href="./Oppa_Favicon.png" />
       </Head>
-
-      <TopMenu />
+    
+      <TopMenu test={ testMe } active={ active } connect={ connect } />
 
       <main className={styles.main}>
         <WalletIndicator />
