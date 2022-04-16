3be5f952ef64614c87738113ac64c086001a3264
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-04-16 22:05:32 +0800

    Implemented staking to contract

--- sections/StakeForm.js
+++ sections/StakeForm.js
@@ -1,12 +1,25 @@
 import React, { useState } from 'react'
 import styles from '../styles/Home.module.css'
+import Web3 from 'web3'
+
+import { STAKING_CONTRACT_ADDRESS } from '../config'
 
 import { GetAllowedStakablePercentage, GetPercentageFromValue } from '../app/utils'
+
+// Contract
+import { OPPAStaking, OPPAtoken} from '../contract'
 
 function StakeForm(props) {
 
 	const [ stakedAmount, setStakedAmount ] = useState('')
 	const [ fieldMessage, setFieldMessage ] = useState('')
+	const [ isApproved, setIsApproved ] = useState(false)
+	const [ message, setMessage ] = useState('')
+	const [ transferHash, setTransferHash ] = useState('')
+	const [ stakingHash, setStakingHash] = useState('')
+	const [ isLoading, setIsLoading ] = useState(false)
+
+	const { account, balance } = props
 
 	const handleChange = (event) => {
 		if (event.target.validity.valid) {
@@ -25,11 +38,34 @@ function StakeForm(props) {
 		setStakedAmount('')
 		setFieldMessage('')
 	}
+
+	const stake = () => {
+		setIsLoading(true)
+		setMessage('progress 0/3')
+		
+		OPPAtoken.methods.approve(account, stakedAmount).send({ from: account }).then(() => {
+			setIsApproved(true)
+			setMessage('progress 1/3')
+			OPPAtoken.methods.transferFrom(account, STAKING_CONTRACT_ADDRESS, stakedAmount).send({ from: account }).then(tokenTransfer => {
+					console.log('DEBUG ...', 'transferFrom', tokenTransfer)
+					setTransferHash(tokenTransfer.blockHash)
+					setMessage('progress 2/3')
+					OPPAStaking.methods.StakeTokens(stakedAmount).send({ from: account }).then(staking => {
+						setStakingHash( staking.blockHash )
+						setMessage(`Staking Complete!`)
+						setStakedAmount('')
+						setMessage('Complete! 3/3')
+						setIsLoading(false)
+					})
+				})
+		})
+	}
 	
 	return (
 		<div className={ styles.summary }>
-			<div className='instructions'>
-				You have not staked any of your OPPA tokens yet. Fill in the form below and enjoy the rewards given every 30 minutes. 
+			<div className={ styles.instructions }>
+				<div>Fill in the form below and enjoy the rewards given every 10 minutes. </div>
+				<div className={ styles.notices } >There will be 3 transactions to sign.</div>
 			</div>
 			<br />
 			<div>
@@ -49,10 +85,14 @@ function StakeForm(props) {
 			</div>
 	
 			<div className={ styles.dashboardActivityButtons }>
-				<button onClick={() => { props.activateStake(true) } }>Stake</button>
+				<button onClick={() => { stake() } }>Stake</button>
 				<a className={ styles.clickable_link } href='#' onClick={() => { resetFields() }} >Reset</a>
 			</div>
-	
+		
+			<div>
+				<div>{ isLoading? 'Validating transactions':'' }</div>
+				<div>{ message }</div>
+			</div>
 		</div>
 	)
 }
