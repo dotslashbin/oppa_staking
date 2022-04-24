f7f3be3e378c3c43521e9b1d1fb52ce104d46c91
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-04-24 16:18:52 +0800

    Removed minutes and read the remaining seconds from store

--- sections/summary.js
+++ sections/summary.js
@@ -5,13 +5,15 @@ import Countdown from 'react-countdown'
 
 function Summary(props) {
 
-	const { stakedAmount } = props
+	const { stakedAmount, nextEpoch } = props
 
-	const renderer = ({ minutes, seconds, completed }) => {
+	const countdownValue = 1000 * Number(nextEpoch)
+
+	const renderer = ({ seconds, completed }) => {
 		if (completed ){
 			return "Reward credited..."
 		} else {
-			return `${ minutes } minutes and ${ seconds } seconds`
+			return `${ seconds } seconds`
 		}
 	}
 
@@ -24,10 +26,10 @@ function Summary(props) {
 				Your current staking delgation: <span className={ styles.highlightedText } >{ stakedAmount }</span>
 			</div>
 			<div>
-				Rewards collected: <span className={ styles.highlightedText } >0</span>
+				Rewards collected: <span className={ styles.highlightedText } >{ props.totalRewards }</span>
 			</div>
 			<div>
-				You will be getting: <span className={ styles.highlightedText }>(WIP)</span> in <Countdown date={ Date.now() + 15000000 } renderer={ renderer } />
+				You will be getting: <span className={ styles.highlightedText }>(WIP)</span> in <Countdown date={ Date.now() + countdownValue } renderer={ renderer } />
 			</div>
 		</div>
 	)
