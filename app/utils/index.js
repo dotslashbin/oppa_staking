f8cf536b5a7f8cbad0654a01d8ce81499f03e08e
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-05-03 16:12:05 +0800

    Created method to get time till next epoch relevant to the frequency

--- app/utils/index.js
+++ app/utils/index.js
@@ -16,3 +16,18 @@ export function GetPercentageFromValue(percentage, totalValue) {
 	const amontToSubtract =  (100 * percentage) / totalValue;
 	return (totalValue - amontToSubtract)
 }
+
+export function GetTimeTillNextEpoch(frequency, startTime) {
+	const timeNow = new Date()
+
+	const startTimeInDate = new Date(0)
+	startTimeInDate.setUTCSeconds(startTime)
+
+	const differenceInSeconds = (timeNow - startTimeInDate) / 1000;
+	const minutes = differenceInSeconds / 60; 
+	const numberOfEpochFrequencies = minutes / frequency
+
+	const remaining = differenceInSeconds - (numberOfEpochFrequencies * frequency)*120
+
+	return remaining
+}
