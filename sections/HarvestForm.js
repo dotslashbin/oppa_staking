7c1a08346740914ae117fdc8236205284be4e3ca
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-05-07 18:22:00 +0800

    Implemented harvest hide

--- sections/HarvestForm.js
+++ sections/HarvestForm.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
-const HarvestForm = (props) => (
-	<div className={ styles.control }>
-		<div>
-			<button onClick={() => { props.unstake() }}>Harvest rewards</button>
+function HarvestForm(props) {
+	
+	const [ showHarvest, setShowHarvest ] = useState(true)
+
+	const handleHarvest = () => {
+		props.unstake()
+		setShowHarvest(false)
+	}
+
+	return (
+		<div className={ styles.control }>
 			<div>
-				<span className={ styles.notices } >Harvesting rewards will undelegate ALL of your tokens</span>
+				{ showHarvest? (<button onClick={ handleHarvest }>Harvest rewards</button>): (<p>Your tokens coming your way! please wait ...</p>) }
+				<div>
+					<span className={ styles.notices } >Harvesting rewards will undelegate ALL of your tokens</span>
+				</div>
 			</div>
 		</div>
-	</div>
-)
+	)
+} 
+
+
 
 export default HarvestForm
