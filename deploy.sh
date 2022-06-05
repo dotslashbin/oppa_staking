425cb1a278df0d3b884e6c0d8abd946c59b64677
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-06-05 09:03:30 +0800

    Updated deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 ./node_modules/.bin/next build && 
-docker build . -t oppa_staking
+docker build . -t oppa_staking &&
+docker tag $(docker images | grep "oppa_staking" | awk '{print $3}' | head -n 1) dotslashbin/oppa_staking && 
+docker push dotslashbin/oppa_staking
