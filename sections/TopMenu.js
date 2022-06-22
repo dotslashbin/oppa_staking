64eec598c40cf0a5eb544036dda6a2fc9d271a2a
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-06-22 13:52:32 +0800

    Added version display

--- sections/TopMenu.js
+++ sections/TopMenu.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import packageJson from '../package.json'
 
 const TopMenu = (props) => (
 	<header className={ styles.topNavBar }>
 		<div className='log-container'>
 			<Image src='/images/Oppa_Logo.png' alt='OPPA logo' height={40} width={150}/>
+			<div className={ styles.versionDisplay } >version: { packageJson.version }</div>
 		</div>
 		<div className='wallet-connector'>
 			<div>
